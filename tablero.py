ee50e55eba8ebc53f1427d9fb93cf265aa9ce364
Tomas Briones <tomasbriones@gmail.com>
2021-03-21 19:39:07 -0300

    adding code

--- tablero.py
+++ tablero.py
@@ -1,1 +1,11 @@
 #create a function that add a new board to play a game 
+def crearTablero(alto,ancho):
+    ready = False
+    tablero = []
+    for i in range(0,alto):
+        for j in range(0,ancho):
+            print("*")
+            tablero.append("*")
+    return ready
+
+    crearTablero(5,6)
