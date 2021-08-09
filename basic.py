216b4537cba93ba0798b4d4e294e9700b5214a37
tomas briones <tomasbriones@gmail.com>
2021-08-08 23:53:32 -0400

    add basic exercises

---
+++ basic.py
@@ -0,0 +1,15 @@
+# def print_hi(name):
+#     print(f'Hello {name}')
+
+# if __name__ == '__main__':
+#     print_hi('tomas')
+# x: str = '10' #no define el tipo de dato
+# print(id(x)) #posicion de memotia donde se almacena x de la literal 10, direccion de memoria 
+# print(type(x))
+
+miNombre = 'Tomas Briones'
+for i in miNombre:
+    if i == " ":
+        print(i)
+    else:
+        print(i,end="")
